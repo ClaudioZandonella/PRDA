d658b71ed04f38abb4c5ec09b69cc98bb6fc4611
Claudio Zandonella <claudiozandonella@gmail.com>
2020-07-09 17:46:04 +0200

    Add tests for Rcpp functions

---
+++ tests/testthat/test_Rcpp_functions.R
@@ -0,0 +1,47 @@
+##################################
+####    Test Rcpp Functions   ####
+##################################
+
+library(PRDAbeta)
+
+#----    input checks    ----
+
+context("Evaluate Rcpp functions")
+
+
+x <- rnorm(3)
+y <- rnorm(3)
+
+x_bis <- rnorm(30)
+y_bis <- rnorm(30)
+
+#----    eval_meanC    ----
+
+test_that("evaluate meanC", {
+  expect_equal(mean(x), meanC(x))
+  expect_equal(mean(y), meanC(y))
+  expect_equal(mean(x_bis), meanC(x_bis))
+  expect_equal(mean(y_bis), meanC(y_bis))
+})
+
+#----    eval_varC    ----
+
+test_that("evaluate varC", {
+  xm <- mean(x)
+  ym <- mean(y)
+  xm_bis <- mean(x_bis)
+  ym_bis <- mean(y_bis)
+
+  expect_equal(var(x), varC(x, xm))
+  expect_equal(var(y), varC(y, ym))
+  expect_equal(var(x_bis), varC(x_bis, xm_bis))
+  expect_equal(var(y_bis), varC(y_bis, ym_bis))
+})
+
+#----    eval_corC    ----
+
+test_that("evaluate corC", {
+  expect_equal(cor(x,y), corC(x, y))
+  expect_equal(cor(x_bis, y_bis), corC(x_bis, y_bis))
+})
+#----
