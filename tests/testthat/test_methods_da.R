689eac0bf6ebabcbaf031de6a3cee7b7321a1150
Claudio Zandonella <claudiozandonella@gmail.com>
2020-09-08 15:56:28 +0200

    covr trial

--- tests/testthat/test_methods_da.R
+++ tests/testthat/test_methods_da.R
@@ -11,15 +11,15 @@ context("Evaluate methods for design analysis")
 
 test_that("evaluate print.design_analysis", {
   # Retrospective
-  verify_output(path = "test_cache/print_retro_cor.txt",
-                retrospective(effect_size = .3, sample_n1 = 20,
-                              B = 50, seed = 2020))
   verify_output(path = "test_cache/print_retro_cohen.txt",
                 retrospective(effect_size = function(x) rnorm(x, .3, .1),
                               sample_n1 = 20, sample_n2 = 20,
                               effect_type = "cohen_d", test_method = "paired",
                               tl = .15, tu = .45, B_effect = 10, B = 50,
                               seed = 2020))
+  verify_output(path = "test_cache/print_retro_cor.txt",
+                retrospective(effect_size = .3, sample_n1 = 20,
+                              B = 50, seed = 2020))
 
   # Prospective
   verify_output(path = "test_cache/print_pro_cor.txt",
